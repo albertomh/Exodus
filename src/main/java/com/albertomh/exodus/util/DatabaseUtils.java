c9097157ccca9e82770d3698cf1ee5808a2c4aa9
Alberto Morón Hernández <alberto.moronh@gmail.com>
2022-05-15 10:09:34 +0100

    Implement the countTables() db utility

--- src/main/java/com/albertomh/exodus/util/DatabaseUtils.java
+++ src/main/java/com/albertomh/exodus/util/DatabaseUtils.java
@@ -4,6 +4,8 @@
 package com.albertomh.exodus.util;
 
 import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public final class DatabaseUtils {
 
@@ -15,7 +17,21 @@ public final class DatabaseUtils {
      * @return A count of existing tables.
      */
     public static Integer countTables(Statement statement) {
-        Integer tableCount = -1;
+        Integer tableCount = 0;
+
+        try {
+            String tableCountSQL = """
+                    SELECT count(*) AS \"table_count\"
+                    FROM information_schema.tables
+                    WHERE LOWER(table_schema) = 'public';""";
+            ResultSet result = statement.executeQuery(tableCountSQL);
+            while (result.next()) {
+                tableCount = result.getInt("table_count");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
         return tableCount;
     }
 
